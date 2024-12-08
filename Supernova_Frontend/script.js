ed11f0c612aa4aa67361983b40aa2549c948b830
Shristi <136554443+SHRISTI-125@users.noreply.github.com>
2024-12-08 13:41:58 +0530

    Add files via upload

---
+++ Supernova_Frontend/script.js
@@ -0,0 +1,8 @@
+function showMessage(message) {
+    alert(message);
+    window.location.href = "supernova_details.html";
+}
+function universe(){
+    window.location.href = "aftermath_supernova.html";
+}
+  
